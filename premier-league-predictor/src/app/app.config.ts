710143ce44e0a70c1dbcceff707f27f36da288ca
Ruthvik Arya <ruthvik.arya@fau.de>
2025-08-13 06:42:25 +0200

    01 - add firebase, tailwindcss, bootstrap homepage with rocket.ai

--- premier-league-predictor/src/app/app.config.ts
+++ premier-league-predictor/src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "pl-predictor-5c8e6", appId: "1:950699719981:web:d658a04a3829c57faee7a4", storageBucket: "pl-predictor-5c8e6.firebasestorage.app", apiKey: "AIzaSyCI1ZLdpz0tjFkUl-npIUTv21gVP35P7QA", authDomain: "pl-predictor-5c8e6.firebaseapp.com", messagingSenderId: "950699719981", measurementId: "G-9BBW5VYNBP" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())
   ]
 };
